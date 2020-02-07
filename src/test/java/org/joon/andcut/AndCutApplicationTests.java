3e6fa57d3c8199bc2827e4cf5b38cb368dc9108f
Peter Boisvert <pboisver@umich.edu>
2020-02-07 13:46:53 -0500

    Initial commit

---
+++ src/test/java/org/joon/andcut/AndCutApplicationTests.java
@@ -0,0 +1,13 @@
+package org.joon.andcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndCutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
