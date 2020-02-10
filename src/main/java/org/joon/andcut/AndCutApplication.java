7968bb32603bb7a03e22d23b276ab156d157fc70
Peter Boisvert <pboisver@umich.edu>
2020-02-10 10:31:06 -0500

    Add start endpoint

--- src/main/java/org/joon/andcut/AndCutApplication.java
+++ src/main/java/org/joon/andcut/AndCutApplication.java
@@ -1,13 +1,24 @@
 package org.joon.andcut;
 
+import java.util.Collections;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AndCutApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AndCutApplication.class, args);
+	}
+
+	@GetMapping
+	public Map<String,String> startPage() {
+
+		return Collections.singletonMap("Hello", "world");
 	}
 
 }
